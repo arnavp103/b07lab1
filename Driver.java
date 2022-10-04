7193dc74a547b86b38cde9799f58432e8e455016
Arnav Priyadarshi <arnavpriyadarshi@Arnavs-MacBook-Pro.local>
2022-10-04 00:26:53 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,17 +1,51 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Driver {
 	public static void main(String[] args) {
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double[] c1 = { 6, 0, 0, 5 };
-		Polynomial p1 = new Polynomial(c1);
-		double[] c2 = { 0, -2, 0, 0, -9 };
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
+		double[] c1 = { 6, 1, 4, 5 };
+		int[] e1 = { 0, 1, 2, 3 };
+		Polynomial p1 = new Polynomial(c1, e1);
+		double[] c2 = { 1, -2, 5, 10, -9 };
+		int[] e2 = { 4, 2, 1, 0, 3 };
+		Polynomial p2 = new Polynomial(c2, e2);
+		Polynomial s = p1.add(p2); // s = 1.0x^4 + 2.0x^2 + 6.0x^1 + 16.0x^0 + -4.0x^3
+		// printPol(s);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
 		if (s.hasRoot(1))
 			System.out.println("1 is a root of s");
 		else
 			System.out.println("1 is not a root of s");
 
+		Polynomial p11 = new Polynomial(new double[] { 8, 3, 1 }, new int[] { 4, 2, 1 });
+		Polynomial p22 = new Polynomial(new double[] { 4, 3, 1 }, new int[] { 3, 2, 1 });
+
+		Polynomial p33 = p11.multiply(p22);
+		printPol(p33); // 32.0x^7 + 24.0x^6 + 20.0^5 + 13.0x^4 + 6.0x^3 + 1x^2
+
+		Polynomial p3 = new Polynomial(new double[] { 5, 4, 3, 1 }, new int[] { 8, 3, 2, 10 });
+		Polynomial p4 = new Polynomial(new double[] { 10, 2, 3, 1 }, new int[] { 10, 8, 6, 1 });
+
+		Polynomial p5 = p3.add(p4);
+		// printPol(p5); // 7x^8 + 4x^3 + 3x^2 + 11x^10 + 3x^6 + x
+
+		File poly = new File("poly.txt");
+		try {
+			Polynomial pf = new Polynomial(poly);
+			printPol(pf);
+		} catch (FileNotFoundException ok) {
+			System.out.println("File not found");
+		}
+
+		p5.saveToFile("poly.txt");
+	}
+
+	private static void printPol(Polynomial p) {
+		for (int i = 0; i < p.coef.length; i++) {
+			System.out.print(p.coef[i] + "x^" + p.exp[i] + " + ");
+		}
+		System.out.println();
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,36 +1,203 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 public class Polynomial {
-	// the 0th index is the constant term, then the indices match the power of x
+	// These are the non-zero coefficients of the polynomial
 	double[] coef;
+	// These represent the integer power of the term of the polynomial, [0, 1, 3]
+	// would refer to a constant + x + x^3. Not necessarily in ascending order.
+	int[] exp;
 
 	public Polynomial() {
 		this.coef = new double[1];
+		this.exp = new int[1];
 	}
 
-	public Polynomial(double[] coef) {
+	public Polynomial(double[] coef, int[] exp) {
 		this.coef = coef;
+		this.exp = exp;
+	}
+
+	// a constructor that takes an argument of type File and reads the one line of
+	// the file to create a polynomial
+	public Polynomial(File poly) throws FileNotFoundException {
+		try {
+			Scanner sc = new Scanner(poly);
+			String line = sc.nextLine();
+			String[] terms = line.split("(?=[-+])");
+			this.coef = new double[terms.length];
+			this.exp = new int[terms.length];
+			for (int i = 0; i < terms.length; i++) {
+				String[] twonums = terms[i].split("x");
+				if (twonums.length == 1) {
+					this.coef[i] = Double.parseDouble(twonums[0]);
+					this.exp[i] = 0;
+				} else {
+					this.coef[i] = Double.parseDouble(twonums[0]);
+					this.exp[i] = Integer.parseInt(twonums[1]);
+				}
+			}
+
+			sc.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+			this.coef = new double[1];
+			this.exp = new int[1]; // if theres as blank file
+		}
+	}
+
+
+	public void saveToFile(String fname) {
+		try {
+			FileWriter output = new FileWriter(fname, false);
+			String[] temp = new String[coef.length * 5];
+			int counter = 0;
+			for (int i = 0; i < coef.length; i++) {
+				if (coef[i] < 0 && i == 0) {
+					temp[counter] = "-";
+					counter++;
+				} else if (coef[i] < 0) {
+					temp[counter-1] = " - ";
+				}
+
+				temp[counter] = String.valueOf(coef[i]);
+				counter++;
+				if (exp[i] == 1) {
+					temp[counter] = "x";
+					counter++;
+				} else if (exp[i] > 1) {
+					temp[counter] = "x";
+					counter++;
+					temp[counter] = String.valueOf(exp[i]);
+					counter++;
+				}
+
+				temp[counter] = "+";
+				counter++;
+			}
+			String[] fin = new String[counter-1];
+			for (int j = 0; j < counter - 1; j++) {
+				fin[j] = temp[j];
+			}
+
+			String line = String.join("", fin);
+			output.write(line);
+
+			output.close();
+		} catch (Exception e) {
+			System.out.println("File not found");
+		}
+
 	}
 
 	// a method to add a polynomial to the current polynomial
 	public Polynomial add(Polynomial p) {
-		double[] newCoef = new double[Math.max(this.coef.length, p.coef.length)];
-		for (int i = 0; i < this.coef.length; i++)
-			newCoef[i] += this.coef[i];
-		for (int i = 0; i < p.coef.length; i++)
-			newCoef[i] += p.coef[i];
-		return new Polynomial(newCoef);
+		int len = this.coef.length + p.coef.length;
+		double[] tempCoef = new double[len];
+		int[] tempExp = new int[len];
+
+		for (int i = 0; i < this.exp.length; i++) {
+			tempCoef[i] = this.coef[i];
+			tempExp[i] = this.exp[i];
+		}
+
+		for (int j = 0; j < p.exp.length; j++) {
+			boolean found = false;
+			for (int k = 0; k < tempExp.length; k++) {
+				if (p.exp[j] == tempExp[k]) {
+					tempCoef[k] += p.coef[j];
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				tempCoef[this.exp.length + j] = p.coef[j];
+				tempExp[this.exp.length + j] = p.exp[j];
+			}
+		}
+
+		// Now we need to remove the zero terms from the array
+		for (int i = 0; i < tempExp.length; i++) {
+			if (tempCoef[i] == 0) {
+				len--;
+			}
+		}
+
+		double newCoef[] = new double[len];
+		int newExp[] = new int[len];
+		int counter = 0;
+		for (int i = 0; i < tempExp.length; i++) {
+			if (tempCoef[i] != 0) {
+				newCoef[counter] = tempCoef[i];
+				newExp[counter] = tempExp[i];
+				counter++;
+			}
+		}
+		return new Polynomial(newCoef, newExp);
 	}
 
 	// a method to evalute a polynomial for a given value of x
 	public double evaluate(double x) {
-		double sum = 0;
+		double result = 0;
 		for (int i = 0; i < this.coef.length; i++) {
-			sum += this.coef[i] * Math.pow(x, i);
+			result += this.coef[i] * Math.pow(x, this.exp[i]);
 		}
-		return sum;
+		return result;
 	}
 
 	public boolean hasRoot(double x) {
 		return this.evaluate(x) == 0;
+	}
+
+	// Returns the result of multiplying the current polynomial by the polynomial p,
+	// should not contain redundant exponents.
+	public Polynomial multiply(Polynomial p) {
+		int len = this.coef.length * p.coef.length;
+		double[] tempCoef = new double[len];
+		int[] tempExp = new int[len];
+		int counter = 0;
+
+		for (int i = 0; i < this.coef.length; i++) {
+			for (int j = 0; j < p.coef.length; j++) {
+				double product = this.coef[i] * p.coef[j];
+				int degree = this.exp[i] + p.exp[j];
+				boolean hasDegree = false;
+				for (int k = 0; k < tempExp.length; k++) {
+					if (degree == tempExp[k]) {
+						tempCoef[k] += product;
+						hasDegree = true;
+						break;
+					}
+				}
+				if (!hasDegree) {
+					tempCoef[counter] = product;
+					tempExp[counter] = degree;
+					counter++;
+				}
+			}
+		}
+
+		// Now we need to remove the zero terms from the array
+		for (int i = 0; i < tempExp.length; i++) {
+			if (tempCoef[i] == 0) {
+				len--;
+			}
+		}
+
+		double newCoef[] = new double[len];
+		int newExp[] = new int[len];
+		int newCounter = 0;
+		for (int i = 0; i < tempExp.length; i++) {
+			if (tempCoef[i] != 0) {
+				newCoef[newCounter] = tempCoef[i];
+				newExp[newCounter] = tempExp[i];
+				newCounter++;
+			}
+		}
+		return new Polynomial(newCoef, newExp);
+
 	}
 
 }
